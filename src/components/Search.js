49fb92586535a83c509443545e3ee07d22d0c655
kokoro <lily.koko412@gmail.com>
2024-03-03 17:33:02 +0900

    commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,9 +8,6 @@ class Search extends React.Component{
       <div className="SearchBar">
       <input type="text" id="search_books" placeholder="本を検索" />
       </div>
-      <div className="AddBooks">
-        <button>+</button>
-      </div>
     </div>
   );
 }
