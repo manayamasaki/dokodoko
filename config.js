29aa0800184ae11893079e30621298bf03f7e080
Kinarii <138790140+Kinarii@users.noreply.github.com>
2024-03-06 22:17:32 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+//postgersql
+module.exports = {
+  database: "mydb_tj0i",
+  user: "umeba",
+  password: "IyVwHPcpnQDdqFVf460Qup10onpwyuN2",
+  host: "dpg-cna9ladjm4es73c792ug-a.oregon-postgres.render.com",
+  ssl: {
+      rejectUnauthorized: false,
+  },
+  max: 10,
+};
+
