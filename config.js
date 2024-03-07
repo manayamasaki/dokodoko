94932421d04b29a0841cd47ee65ee731a801fd75
umeba <84313074+umeba02@users.noreply.github.com>
2024-03-07 18:28:55 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 //config.js
 //postgersql
 module.exports = {
-        
+    database: PGNAME,
+    user: PGUSER,
+    password: PGPASS,
+    host: PGHOST,
+  
+    ssl: {
+      rejectUnauthorized: false
+    },
+    max: 10,
 };
 
